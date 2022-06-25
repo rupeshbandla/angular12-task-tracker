83bfdcfa7ef03fb1aea7c0db9c1174a980526457
rupeshbandla <rupeshreddy451@gmail.com>
2022-06-25 15:08:38 +0530

    final code

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -30,7 +30,7 @@ export class TasksComponent implements OnInit {
 
   addTask(task: Task) {
     this.taskService.addTask(task).subscribe(task => {
-      // this.tasks.push(task);
+      // this.tasks.push(task); 
     })
   }
 
